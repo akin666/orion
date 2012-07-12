2f1541504b49752f0b3a18d09a3b25ab464da084
akin <akin@loth.P-661HNU-F1>
2012-07-12 09:26:46 +0300

    added

---
+++ aries/property/uilocation.cpp
@@ -0,0 +1,21 @@
+/*
+ * uilocation.cpp
+ *
+ *  Created on: 12.7.2012
+ *      Author: akin
+ */
+
+#include "uilocation.hpp"
+
+namespace orion
+{
+
+UILocation::UILocation()
+{
+}
+
+UILocation::~UILocation()
+{
+}
+
+} // namespace orion
---
+++ aries/property/uilocation.hpp
@@ -0,0 +1,22 @@
+/*
+ * uilocation.hpp
+ *
+ *  Created on: 12.7.2012
+ *      Author: akin
+ */
+
+#ifndef UILOCATION_HPP_
+#define UILOCATION_HPP_
+
+namespace orion
+{
+
+class UILocation
+{
+public:
+	UILocation();
+	virtual ~UILocation();
+};
+
+} // namespace orion
+#endif // UILOCATION_HPP_
